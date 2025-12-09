bc6570cc5c6478d3a6970cfc5c1d24d4981c1e39
Pandahoccode <117577917+Pandahoccode@users.noreply.github.com>
2025-12-09 17:38:50 +0100

    update : function API & Fetch API

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from API import get_api_key, fetch_data_from_api
+
 def main():
     print("Hello from sae-python!")
-
+    WEATHER_API = get_api_key("WEATHERBIT_TOKEN")
+    fetch_data_from_api(url="https://api.weatherbit.io/v2.0/current ",
+                      params={"city":"Lille","key":WEATHER_API})
 
 if __name__ == "__main__":
     main()
